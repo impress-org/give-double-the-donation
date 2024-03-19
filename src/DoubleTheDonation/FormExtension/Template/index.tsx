7590422979d4b034d1ccc08c3f43f4ec7c6b6bfa
Ante Laca <ante.laca@get10up.com>
2024-03-19 13:21:46 +0100

    feature: update template

--- src/DoubleTheDonation/FormExtension/Template/index.tsx
+++ src/DoubleTheDonation/FormExtension/Template/index.tsx
@@ -1,14 +1,18 @@
+import {__} from '@wordpress/i18n';
+import './styles.scss';
+
 /**
  * @unreleased
  */
-
 function Template() {
     return (
-        <div>
-            <h1>DTD template</h1>
+        <div className="give-dtd-block-template">
+            {__('This block requires additional setup. Go to settings to connect your account.', 'give-double-the-donation')}
         </div>
     );
 }
+
+export default Template;
 
 // @ts-ignore
 window.givewp.form.templates.groups.dtd = Template;
