c2d371955edc6a589c9a39ae065cb77a41a977f3
Ante Laca <ante.laca@get10up.com>
2024-03-20 14:58:17 +0100

    feature: create types file

---
+++ src/DoubleTheDonation/FormExtension/Template/types.ts
@@ -0,0 +1,7 @@
+export type Company = {
+    id: string;
+    company_name: string;
+    matching_gift_offered: boolean;
+    minimum_matched_amount?: number;
+    maximum_matched_amount?: number;
+}
