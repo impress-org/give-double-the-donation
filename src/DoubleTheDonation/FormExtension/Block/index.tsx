1c6658af22d89097f7fff927a2b09fcfbf0b8231
Ante Laca <ante.laca@get10up.com>
2024-03-21 10:30:36 +0100

    feature: edit block component

--- src/DoubleTheDonation/FormExtension/Block/index.tsx
+++ src/DoubleTheDonation/FormExtension/Block/index.tsx
@@ -1,14 +1,9 @@
 import block from './block.json';
+import Edit from './Edit';
 
 // @ts-ignore
 window.givewp.form.blocks.register(block.name, {
     ...block,
-    edit: () => {
-        return (
-            <div>
-                DTD Block
-            </div>
-        )
-    },
+    edit: Edit,
     save: () => null,
 });
