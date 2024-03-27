1bd56b5badcdcf44df9fbf3b5a0efb343e47394e
Ante Laca <ante.laca@get10up.com>
2024-03-27 11:42:00 +0100

    refactor: remove preformatted text

--- src/DoubleTheDonation/resources/views/dtd-receipt.php
+++ src/DoubleTheDonation/resources/views/dtd-receipt.php
@@ -21,16 +21,5 @@ $getLink = function ($url, $text) {
     }
 };
 
-$getProcess = function ($process) {
-    if ( ! empty($process)) {
-        return sprintf('<pre class="" style="text-align:left; padding:20px; max-height:250px; overflow-y:auto; border:1px solid #f2f2f2;">%s</pre>', $process) . PHP_EOL;
-    }
-};
-
-if (empty($instructions['url_guidelines']) && empty($instructions['url_forms'])) {
-    echo $getProcess($instructions['matching_process']);
-} else {
-    echo $getLink($instructions['url_guidelines'], __('Match Guidelines', 'give-double-the-donation'));
-    echo $getLink($instructions['url_forms'], __('Complete the Match', 'give-double-the-donation'));
-}
-
+echo $getLink($instructions['url_guidelines'], __('Match Guidelines', 'give-double-the-donation'));
+echo $getLink($instructions['url_forms'], __('Complete the Match', 'give-double-the-donation'));
