4c8d85896034f085fb302558e28f06d8c0b1c83f
Ante Laca <ante.laca@gmail.com>
2024-12-11 21:18:05 +0100

    Fix: missing company id (#23)
    * refactor: remove redundant meta update action
    
    * refactor: get correct dtd block data
    
    * Revert "refactor: remove redundant meta update action"
    
    This reverts commit 46506e9276c8c17a589feea1cb08da3c92f39a45.
    
    * refactor: bailout if form is v3 form
    
    * refactor: data handling for v2 and v3 forms
    
    * refactor: use DonationNote

--- src/DoubleTheDonation/Payment.php
+++ src/DoubleTheDonation/Payment.php
@@ -14,13 +14,20 @@ class Payment {
 	 */
 	public function addPaymentMeta( $payment_id, $payment_data ) {
 
-		$companyID = isset( $_POST['doublethedonation_company_id'] ) ? give_clean( $_POST['doublethedonation_company_id'] ) : '';
+        $companyID = isset( $_POST['doublethedonation_company_id'] )
+            ? give_clean( $_POST['doublethedonation_company_id'] )
+            : '';
+        $companyName = isset( $_POST['doublethedonation_company_name'] )
+            ? give_clean( $_POST['doublethedonation_company_name'] )
+            : '';
+        $companyEnteredText = isset( $_POST['doublethedonation_entered_text'] )
+            ? give_clean( $_POST['doublethedonation_entered_text'] )
+            : '';
+
 		if ( ! $companyID ) {
 			return false;
 		}
 
-		$companyName         = isset( $_POST['doublethedonation_company_name'] ) ? give_clean( $_POST['doublethedonation_company_name'] ) : '';
-		$companyEnteredText = isset( $_POST['doublethedonation_entered_text'] ) ? give_clean( $_POST['doublethedonation_entered_text'] ) : '';
 
 		give_update_meta( $payment_id, 'doublethedonation_company_id', $companyID );
 		give_update_meta( $payment_id, 'doublethedonation_company_name', $companyName );
@@ -69,7 +76,7 @@ class Payment {
 			$data_360['doublethedonation_company_id'] = $companyID;
 		}
 
-		$companyEnteredText = isset( $paymentMeta['doublethedonation_company_id'] ) ? $paymentMeta['doublethedonation_company_id'] : '';
+		$companyEnteredText = isset( $paymentMeta['doublethedonation_entered_text'] ) ? $paymentMeta['doublethedonation_entered_text'] : '';
 		if ( $companyEnteredText ) {
 			$data_360['doublethedonation_entered_text'] = $companyEnteredText;
 		}
