56313a36f980d212ae9ab1fc1281cd310d851ecf
Paulo Iankoski <me@wpdev.com.br>
2026-01-06 16:25:09 -0300

    doc: add docblocks for action handlers

--- src/DoubleTheDonation/Actions/RegisterDonationOnDTD.php
+++ src/DoubleTheDonation/Actions/RegisterDonationOnDTD.php
@@ -7,20 +7,26 @@ use Give\Helpers\Form\Utils;
 use Give\Log\Log;
 use GiveDoubleTheDonation\DoubleTheDonation\Payment;
 
+/**
+ * @unreleased
+ */
 class RegisterDonationOnDTD {
+    /**
+     * Called on give_insert_payment action.
+     *
+     * @unreleased
+     */
     public function __invoke($payment_id, $payment_data) {
         // V3 forms are handled by the field scope
         if (!isset($_POST) || Utils::isV3Form((int)$payment_data['give_form_id'])) {
-            if (defined('WP_DEBUG') && WP_DEBUG) {
-                Log::warning(
-                    'Double the Donation: Skipping donation registration for V3 form',
-                    [
-                        'category' => 'Payment',
-                        'source' => 'Double the Donation add-on',
-                        'payment_id' => $payment_id,
-                    ]
-                );
-            }
+            Log::info(
+                'Double the Donation: Skipping donation registration for V3 form',
+                [
+                    'category' => 'Payment',
+                    'source' => 'Double the Donation add-on',
+                    'payment_id' => $payment_id,
+                ]
+            );
 
             return;
         }
--- src/DoubleTheDonation/Actions/RegisterRecurringDonationOnDTD.php
+++ src/DoubleTheDonation/Actions/RegisterRecurringDonationOnDTD.php
@@ -5,7 +5,15 @@ namespace GiveDoubleTheDonation\DoubleTheDonation\Actions;
 use GiveDoubleTheDonation\DoubleTheDonation\Payment;
 use Give_Payment;
 
+/**
+ * @unreleased
+ */
 class RegisterRecurringDonationOnDTD {
+    /**
+     * Called on give_recurring_record_payment action.
+     *
+     * @unreleased
+     */
     public function __invoke(Give_Payment $payment) {
         give(Payment::class)->addDonationToDTD($payment->ID, $payment);
     }
